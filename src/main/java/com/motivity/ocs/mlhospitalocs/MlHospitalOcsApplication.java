df8c8d6387015dcd6592827174219be10ad999f6
Hari <hari.sana@motivitylabs.com>
2023-02-10 15:09:02 +0530

    Initial commit

---
+++ src/main/java/com/motivity/ocs/mlhospitalocs/MlHospitalOcsApplication.java
@@ -0,0 +1,13 @@
+package com.motivity.ocs.mlhospitalocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlHospitalOcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlHospitalOcsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/motivity/ocs/mlhospitalocs/MlHospitalOcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motivity.ocs.mlhospitalocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlHospitalOcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
